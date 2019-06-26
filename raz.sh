5ec760a14d98dc323144816cce81c3737f157c0b
viruss90 <52212819+viruss90@users.noreply.github.com>
2019-06-27 00:18:12 +0800

    Add files via upload

---
+++ raz.sh
@@ -0,0 +1,118 @@
+clear
+figlet -f slant "welcome"|lolcat
+echo
+echo
+echo 
+echo "+===============================+" | lolcat
+echo "| Nickname : ༆⍟мя.α∂sツ         |" | lolcat
+echo "| WhatsApp : 0895*******97      |" | lolcat
+echo "| Facebook : Adit Saputra       |" | lolcat
+echo "+===============================+" | lolcat
+echo
+toilet -f future "  SANTUYY  " | lolcat
+echo   " Silahkan dipilih : " | lolcat
+echo "+===========================+" | lolcat
+echo "+[1].KUOTA MURAH     |  ༆  |+" | lolcat
+echo "+[2].DDOS            |  ⍟  |+" | lolcat
+echo "+[3].AUTO REPORT     |  м  |+" | lolcat
+echo "+[4].PROFIL GUARD FB |  я  |+" | lolcat
+echo "+[5].BOT IG          |  .  |+" | lolcat
+echo "+[6].HACK FB TARGET  |  α  |+" | lolcat
+echo "+[7].SPAM WA         |  ∂  |+" | lolcat
+echo "+[8].SPAM SMS        |  s  |+" | lolcat
+echo "+[0].keluar          |  ツ |+" | lolcat
+echo "+===========================+" | lolcat
+echo "╭─[pilih]" | lolcat
+read -p "╰─>" p
+
+if [ $p = 1 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/sumar-kun/KuotaMurah
+cd KuotaMurah
+sh 5um4r.sh
+fi
+
+if [ $p = 2 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+cd DOS-TOOKITS
+sh DoS-TOOKIT.sh
+fi
+
+if [ $p = 3 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $p = 4 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfilGuardFb
+php guard.php
+fi
+
+if [ $p = 5 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/ikiganteng/bot-igeh
+cd bot-igeh
+unzip node_modules.zip
+node index.js
+fi
+
+if [ $p = 6 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/mbest99/MIXINGS.git
+cd MIXINGS
+bash oppay.sh
+fi
+
+if [ $p = 7 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/TobzCyberTeam/SpamWa
+cd SpamWa
+php SpamWa.php
+fi
+
+if [ $p = 8 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+steep1
+git clone https://github.com/joss24242/SpamSms
+cd SpamSms
+python2 mantan.py
+fi
+
+if [ $p = 0 ]
+then
+clear
+figlet -f slant "EXIT"|lolcat
+echo "Thanks telah menggunakan tools ini"
+echo 
+echo "bye.. BosQ"
+exit
+fi
